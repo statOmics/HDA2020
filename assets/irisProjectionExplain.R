08848d9a05fac3cdbc22e08b3d6cd60b78196972
statOmics <lieven.clement@ugent.be>
2020-10-27 15:11:43 +0100

    H3 prediction

---
+++ assets/irisProjectionExplain.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+irisSetosa <- iris %>%
+  filter(Species == "setosa") %>%
+  select("Sepal.Length","Sepal.Width")
+
+
+nIris <- nrow(irisSetosa)
+hIris <- diag(nIris) - matrix(1/nIris, nIris, nIris)
+irisX <- irisSetosa %>%
+  as.matrix
+irisX <- hIris %*% irisX
+
+irisSvd <- svd(irisX)
+
+pIrisOrig <- irisSetosa %>%
+  ggplot(aes(x=Sepal.Length,y=Sepal.Width)) +
+  geom_point() +
+  xlim(range(c(irisSetosa[,1],irisX[,1]))) +
+  ylim(range(c(irisSetosa[,2],irisX[,2])))
+
+pIrisOrig +
+  ggtitle("original")
+
+pIris <- irisX %>%
+  as.data.frame %>%
+  ggplot(aes(x=Sepal.Length,y=Sepal.Width)) +
+  geom_point()
+
+pIris +
+  xlim(range(c(irisSetosa[,1],irisX[,1]))) +
+  ylim(range(c(irisSetosa[,2],irisX[,2]))) +
+  ggtitle("centered")
+
+pIris +
+  geom_segment(
+    aes(
+      x = 0,
+      y = 0,
+      xend = -irisSvd$v[1,1],
+      yend = -irisSvd$v[2,1]
+      ),
+      arrow = arrow(length=unit(0.4,"cm"))
+    ) +
+  geom_segment(
+    aes(
+      x = 0,
+      y = 0,
+      xend = irisSvd$v[1,2],
+      yend = irisSvd$v[2,2]
+      ),
+    arrow = arrow(length=unit(0.4,"cm"))
+    ) +
+  geom_text(
+    aes(
+      x = -irisSvd$v[1,1]*1.2,
+      y = -irisSvd$v[2,1]*1.2,
+      label="v1"
+      )
+    ) +
+  geom_text(
+    aes(
+      x = irisSvd$v[1,2]*1.2,
+      y = irisSvd$v[2,2]*1.2,
+      label="v2"
+      )
+    ) +
+  xlim(-1, 1) +
+  ylim(-1,1)
--- install.R
+++ install.R
@@ -1,14 +1,16 @@
 ## Install dependencies
 install.packages(c("remotes", "BiocManager"), Ncpus = 2L)
 
-BiocManager::install(c(
-  "tidyverse", "devtools", "rmarkdown", "knitr",
-  "plot3D", "git2r", "downloader", "imager",
-  "pixmap", "gridExtra", "ggmap",
-  "ExperimentHub", "scater",
-  "CCA"
-), Ncpus = 2L)
+BiocManager::install(
+  c(
+    "tidyverse", "devtools", "rmarkdown", "knitr",
+    "plot3D", "git2r", "downloader", "imager",
+    "pixmap", "gridExtra", "ggmap","ExperimentHub",
+    "scater", "CCA", "glmnet", "ggforce",
+    "latex2exp", "SemiPar", "glmnet", "DAAG",
+    "genefu", "breastCancerMAINZ", "plotROC"
+  ),
+  Ncpus = 2L)
 
 remotes::install_github("hadley/emo")
 remotes::install_github("vqv/ggbiplot")
-
