31a6fc5a507db19762775033f4b24963c6806af3
statOmics <lieven.clement@ugent.be>
2020-11-02 15:08:23 +0100

    merged install.R

---
+++ install.R
@@ -0,0 +1,17 @@
+## Install dependencies
+install.packages(c("remotes", "BiocManager"), Ncpus = 2L)
+
+BiocManager::install(
+  c(
+    "tidyverse", "devtools", "rmarkdown", "knitr",
+    "plot3D", "git2r", "downloader", "imager",
+    "pixmap", "gridExtra", "ggmap", "ExperimentHub",
+    "scater", "CCA", "glmnet", "ggforce",
+    "latex2exp", "SemiPar", "DAAG",
+    "genefu", "breastCancerMAINZ", "plotROC", "PMA",
+    "mgcv", "sessioninfo","sparseLDA", "pls", "NormalBetaPrime", "boot"
+  ),
+  Ncpus = 2L)
+
+remotes::install_github("hadley/emo")
+remotes::install_github("vqv/ggbiplot")
