50698b279df8c18b7e6bf047ed4bcb59121a86ae
statOmics <lieven.clement@ugent.be>
2020-12-10 18:44:45 +0100

    added package plotly to install.R  for GeomInterpretation

--- install.R
+++ install.R
@@ -10,7 +10,7 @@ BiocManager::install(
     "latex2exp", "SemiPar", "DAAG",
     "genefu", "breastCancerMAINZ", "plotROC", "PMA",
     "mgcv", "sessioninfo","sparseLDA", "pls", "NormalBetaPrime", "boot",
-    "cluster", "locfdr", "gganimate","magick"
+    "cluster", "locfdr", "gganimate","magick", "plotly"
   ),
   Ncpus = 2L)
 
